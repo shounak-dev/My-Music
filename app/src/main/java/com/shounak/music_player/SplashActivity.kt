e81e4f767fc4f7a6174a637c8d7223d4789c32d8
SHOUNAK DAS <shounakdas30@gmail.com>
2022-02-03 18:21:29 +0530

    Add Splash Screen

---
+++ app/src/main/java/com/shounak/music_player/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.shounak.music_player
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
